a5c039d799fe0018c61b4afcd758608ef581dd3f
MaGaKZ <shokparbayev@gmail.com>
2014-07-11 13:08:28 +0000

    SignUp SignIn SignOut

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+gem 'bcrypt-ruby', '3.1.2'
 # gem 'therubyracer',  platforms: :ruby
 gem 'bootstrap-sass', '2.3.2.0'
 # Use jquery as the JavaScript library
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   resources :microposts
-
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
  
   match '/home', to: 'static_pages#home', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
   match '/signup',  to: 'users#new',      via: 'get'
+  match '/signin',  to: 'sessions#new',   via: 'get'
+  match '/signout', to: 'sessions#destroy',via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
