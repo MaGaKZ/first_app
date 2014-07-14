0536281a979f12a790633808fcd3a2b3bab1d280
MaGaKZ <shokparbayev@gmail.com>
2014-07-14 19:57:04 +0000

    I have done this project

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,7 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
   def feed
-    # Это предварительное решение. См. полную реализацию в "Following users".
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
